0e59a3097c8a5139aaac1624072600504db32dc8
Lauren Carne <lauren@picnic.ventures>
2023-07-13 11:00:23 +0100

    DayThree part two

--- advent/03-RucksackReorganization/day02.ts
+++ advent/03-RucksackReorganization/day02.ts
@@ -15,8 +15,17 @@ function getPriorityScore(letter: string): number {
   return alpha.indexOf(letter) + 1
 }
 
+function commonLetter(bagOne: string[], bagTwo: string[], bagThree: string[]): string | undefined {
+  for (let i = 0; i < bagOne.length; i++) {
+    if (bagTwo.includes(bagOne[i]) && bagThree.includes(bagOne[i])) {
+      return bagOne[i]!
+    }
+  }
+}
+
 export function DayThree() {
-  let totalScore = 0
+  // Part One
+  let totalScorePartOne = 0
 
   bags.forEach((bag) => {
     const bagOne = bag.splice(0, bag.length / 2)
@@ -25,9 +34,27 @@ export function DayThree() {
     if (duplicate == null) return
 
     const answer = getPriorityScore(duplicate)
-    totalScore = totalScore + answer
-    console.log('Your total score for part one is: ', totalScore)
+    totalScorePartOne = totalScorePartOne + answer
   })
+
+  // Part Two
+  let totalScorePartTwo = 0
+
+  for (let i = 0; i < bags.length; i += 3) {
+    const bagOne = bags[i]
+    const bagTwo = bags[i + 1]
+    const bagThree = bags[i + 2]
+
+    const common = commonLetter(bagOne, bagTwo, bagThree)
+
+    if (common == null) return
+
+    const answer = getPriorityScore(common)
+    totalScorePartTwo = totalScorePartTwo + answer
+  }
+
+  console.log('Your total score for part one is: ', totalScorePartOne)
+  console.log('Your total score for part two is: ', totalScorePartTwo)
 }
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'
