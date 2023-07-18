8dda2a95a68dded8e1c6fee192592a76eccd4c10
Lauren Carne <lauren@picnic.ventures>
2023-07-18 12:40:49 +0100

    Day Six, part Two

--- advent/06-TuningTrouble/day06.ts
+++ advent/06-TuningTrouble/day06.ts
@@ -6,7 +6,7 @@ function hasRepeats(str: string) {
   return /(.).*\1/.test(str)
 }
 
-export function DaySix() {
+function PartOne() {
   let answerPartOne = null
   for (let i = 0; i < lines.length; i++) {
     if (!hasRepeats(lines.slice(i, i + 4))) {
@@ -14,4 +14,19 @@ export function DaySix() {
       return console.log('Your total score for part one is: ', answerPartOne)
     }
   }
+}
+
+function PartTwo() {
+  let answerPartTwo = null
+  for (let i = 0; i < lines.length; i++) {
+    if (!hasRepeats(lines.slice(i, i + 14))) {
+      answerPartTwo = i + 14
+      return console.log('Your total score for part two is: ', answerPartTwo)
+    }
+  }
+}
+
+export function DaySix() {
+  PartOne()
+  PartTwo()
 }
